48990b59f65e04643c5c2b84134769ffe053f9bd
asus <asus@DESKTOP-5SUGM37>
2026-04-26 22:48:00 -0400

    Agregar archivos de proyecto.

---
+++ galos/galos.py
@@ -0,0 +1,78 @@
+
+#valla esto no lo sabia 
+from time import sleep
+from turtle import delay
+print ("No bore lo de arriba profe")
+# osea que deyal es mas para la robotica mientras el sleep es para la programacion
+
+print ("hola mundo")
+caja = 9
+caja2 = 3
+print (caja + caja2)
+def eras_one():
+ print("hola")
+eras_one()
+# bieeeeen 
+def saludar(nombre):
+    print("hola",nombre)
+llamada= saludar("felipe")
+print (llamada)
+def test(mun_1,mun_2,mun_3):
+    return (mun_1 + mun_2 + mun_3)/3
+
+llamada1 = test(2,2,5)
+print (llamada1)
+sleep(10)
+print("testeo de a no es delay es sleep")
+sleep(25)
+import time
+
+print("inicio del programa")
+
+tiempo = 35
+
+while tiempo >= 0:
+    print(tiempo)
+
+    tiempo -=1
+    time.sleep(1)
+
+print (" crees esto acabo jeje no")
+sleep = 5
+# aaaaa me esta gustando esto
+print ("recuerda el ultimo rie mejor")
+
+def resta(num_1,num_2):
+    return num_1 - num_2
+
+print(resta(num_1=5,num_2=8))
+
+
+
+sleep = 60
+
+
+
+
+#intento de interfaz de usuario 0.1
+print ("acaso es necesario utilizar ia por que yo no quero")
+import tkinter as tk
+
+def imprimir_mensaje():
+    print("hola mundo")
+
+
+def ya_me_estoy_aburriendo():
+    texto=entrada.get()
+    print(f"texto ingresado {texto}")
+# crea ventana 
+ventana =tk.Tk()
+ventana.title("galos premiun")
+#crear boton
+boton = tk.Button(ventana,text="haz click",command=imprimir_mensaje)
+boton.pack()
+#cre
+#mostrar ventana
+ventana.mainloop()
+sleep = 5
+print("yipi =)")
